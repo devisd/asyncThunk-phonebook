510175cfbb3a414c73243a81da11ab7f57b57cc8
devisd <89788799+devisd@users.noreply.github.com>
2022-11-14 01:19:22 +0700

    all task completed

---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+import contactsSlice from './slices/contactSlice';
+import filterSlice from './slices/filterSlice';
+import logger from 'redux-logger';
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsSlice.reducer,
+    filter: filterSlice.reducer,
+  },
+  middleware: getDefaultMiddleware => [...getDefaultMiddleware(), logger],
+
+  devTools: process.env.NODE_ENV === 'development',
+});
+
+export const { add, remove } = contactsSlice.actions;
+export const { filter } = filterSlice.actions;
